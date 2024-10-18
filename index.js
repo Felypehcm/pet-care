ae3726de3d8c83f9059623b2e80e0497b4ccb626
Felype Cabral <felypehcm@gmail.com>
2024-10-18 13:11:38 -0300

    first commit

---
+++ index.js
@@ -0,0 +1,11 @@
+/*PatCare - O App Completo para Tutores e Pets
+PatCare é uma plataforma inovadora que centraliza todas as necessidades do seu pet em um único lugar. Desenvolvida para facilitar a vida dos tutores e donos de estabelecimentos pet-friendly, o app oferece uma ampla gama de funcionalidades:
+
+Serviços de Pet Shop: Reserve banhos, tosas e outros serviços com facilidade.
+Marketplace: Compre produtos para seu pet de forma prática e segura.
+Histórico Veterinário: Mantenha registros atualizados de vacinas, consultas e tratamentos.
+Estabelecimentos Pet-Friendly: Descubra lugares que aceitam seu pet.
+Gamificação: Participe de desafios e ganhe prêmios e bônus exclusivos.
+Sistema de Acompanhamento: Veja relatórios de serviços e mantenha o bem-estar do seu pet sempre em dia.
+O PatCare é projetado tanto para tutores de pets quanto para negócios do setor pet, promovendo integração e facilidade para ambos.
+*/
